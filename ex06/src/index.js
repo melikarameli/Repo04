f904a285aaeae044c181ba0843182641734d5adf
melikarameli <melikahatunic2001@gmail.com>
2022-04-01 09:26:28 +0200

    Added ex06

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,6 +1,6 @@
 function main(numOne, numTwo){
     // only change code below this line
-    myQuotient = numOne / numTwo;
+    var myQuotient = numOne / numTwo;
     // only change code above this line
     return myQuotient;
 }
